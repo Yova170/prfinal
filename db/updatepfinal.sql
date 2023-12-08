90ac4ae1d436b30014e83155c87a6e0fa6cc28ce
Darren Araúz <88292002+darrenarauz21@users.noreply.github.com>
2023-12-08 12:24:47 -0500

    Update updatepfinal.sql

--- db/updatepfinal.sql
+++ db/updatepfinal.sql
@@ -10,6 +10,8 @@ MODIFY COLUMN `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `productos`
 ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
+ALTER TABLE productos
+MODIFY COLUMN id_producto INT AUTO_INCREMENT;
 
 
 commit;
